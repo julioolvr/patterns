c9445fece615d2559d036e470c09ce984dbfe5d8
Julio <julio.olvr@gmail.com>
2024-04-26 19:14:37 +0200

    Paint with different colors

--- src/Pattern.tsx
+++ src/Pattern.tsx
@@ -14,13 +14,14 @@ class Palette {
   }
 }
 
-function usePattern(width: number, height: number, initialPalette: Palette) {
-  const [palette] = useState(initialPalette);
+function usePattern(width: number, height: number) {
+  const [palette] = useState(new Palette());
   const [pixels, setPixels] = useImmer(
     R.times(height, () => R.times(width, R.constant(0)))
   );
 
   return {
+    palette,
     colorGrid(): Array<Array<Color>> {
       return pixels.map((row) => {
         return row.map((paletteIndex) => palette.colors[paletteIndex]);
@@ -34,23 +35,73 @@ function usePattern(width: number, height: number, initialPalette: Palette) {
   };
 }
 
-export default function PatternUi() {
-  const pattern = usePattern(10, 20, new Palette());
-
+function PatternUi({ colorGrid, onPaint }: PatternUiProps) {
   return (
     <div className="pattern">
-      {pattern.colorGrid().map((row, y) => (
+      {colorGrid.map((row, y) => (
         <div key={y} className="pattern--row">
           {row.map((color, x) => (
             <button
               key={x}
               className="pattern--cell"
               style={{ backgroundColor: color }}
-              onClick={() => pattern.setColor(1, x, y)}
+              onClick={() => onPaint(x, y)}
             ></button>
           ))}
         </div>
       ))}
+    </div>
+  );
+}
+
+type PatternUiProps = {
+  colorGrid: Array<Array<Color>>;
+  onPaint: (x: number, y: number) => void;
+};
+
+function PaletteSelector({
+  palette,
+  selectedColorIndex,
+  onSelectColorIndex,
+}: PaletteSelectorProps) {
+  return (
+    <div className="palette-selector">
+      {palette.colors.map((color, index) => (
+        <button
+          key={index}
+          onClick={() => onSelectColorIndex(index)}
+          style={
+            selectedColorIndex === index ? { border: "3px solid black" } : {}
+          }
+        >
+          {color}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+type PaletteSelectorProps = {
+  palette: Palette;
+  selectedColorIndex: number;
+  onSelectColorIndex: (index: number) => void;
+};
+
+export default function Editor() {
+  const [currentColorIndex, setCurrentColorIndex] = useState(0);
+  const pattern = usePattern(10, 20);
+
+  return (
+    <div>
+      <PaletteSelector
+        palette={pattern.palette}
+        selectedColorIndex={currentColorIndex}
+        onSelectColorIndex={setCurrentColorIndex}
+      />
+      <PatternUi
+        colorGrid={pattern.colorGrid()}
+        onPaint={(x, y) => pattern.setColor(currentColorIndex, x, y)}
+      />
     </div>
   );
 }
