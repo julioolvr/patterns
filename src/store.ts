ce7e2b1cdd767c2f32e671412b1d8924d376d153
Julio <julio.olvr@gmail.com>
2024-04-27 18:13:19 +0200

    Persist storage

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,7 @@
 import * as R from "remeda";
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
+import { createJSONStorage, persist } from "zustand/middleware";
 import tinycolor from "tinycolor2";
 
 import { Color } from "./modules/palette";
@@ -23,31 +24,59 @@ type Actions = {
 };
 
 const useStore = create<State & Actions>()(
-  immer((set) => ({
-    pattern: {
-      palette: [tinycolor("white"), tinycolor("red"), tinycolor("black")],
-      pixels: R.times(20, () => R.times(10, R.constant(0))),
-    },
-    ui: {
-      isPatternShifted: true,
-    },
-    togglePatternShift: () =>
-      set((state) => {
-        state.ui.isPatternShifted = !state.ui.isPatternShifted;
+  persist(
+    immer((set) => ({
+      pattern: {
+        palette: [tinycolor("white"), tinycolor("red"), tinycolor("black")],
+        pixels: R.times(20, () => R.times(10, R.constant(0))),
+      },
+      ui: {
+        isPatternShifted: true,
+      },
+      togglePatternShift: () =>
+        set((state) => {
+          state.ui.isPatternShifted = !state.ui.isPatternShifted;
+        }),
+      setPixelColor: (colorIndex, x, y) =>
+        set((state) => {
+          state.pattern.pixels[y][x] = colorIndex;
+        }),
+      addPaletteColor: (color) =>
+        set((state) => {
+          state.pattern.palette.push(color);
+        }),
+      updatePaletteColor: (colorIndex, color) =>
+        set((state) => {
+          state.pattern.palette[colorIndex] = color;
+        }),
+    })),
+    {
+      name: "pattern-storage",
+      storage: createJSONStorage(() => localStorage, {
+        reviver: (_key, value) => {
+          if (
+            value &&
+            typeof value === "object" &&
+            "type" in value &&
+            value.type === "tinycolor" &&
+            "color" in value &&
+            typeof value.color === "string"
+          ) {
+            return tinycolor(value.color);
+          }
+
+          return value;
+        },
+        replacer: (_key, value) => {
+          if (value instanceof tinycolor) {
+            return { type: "tinycolor", color: value.toHex8String() };
+          }
+
+          return value;
+        },
       }),
-    setPixelColor: (colorIndex, x, y) =>
-      set((state) => {
-        state.pattern.pixels[y][x] = colorIndex;
-      }),
-    addPaletteColor: (color) =>
-      set((state) => {
-        state.pattern.palette.push(color);
-      }),
-    updatePaletteColor: (colorIndex, color) =>
-      set((state) => {
-        state.pattern.palette[colorIndex] = color;
-      }),
-  }))
+    }
+  )
 );
 
 export default useStore;
