458264976bed05b763ca81ade9de278c62c89621
Julio <julio.olvr@gmail.com>
2024-05-29 21:21:06 +0200

    Remove fake await for loading patterns

--- src/queries/patterns.ts
+++ src/queries/patterns.ts
@@ -1,19 +1,5 @@
-import { useQuery } from "@tanstack/react-query";
-
 import client from "../db/client";
 
-function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 export async function listPatterns() {
-  await sleep(5000);
   return client.from("patterns").select();
-}
-
-export function usePatternsListQuery() {
-  return useQuery({
-    queryKey: ["patterns"],
-    queryFn: listPatterns,
-  });
 }
