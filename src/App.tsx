cd6352e5a49515388151b857dd0688ace7a71813
Julio <julio.olvr@gmail.com>
2024-05-29 20:47:12 +0200

    Add tanstack-query devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { MantineProvider } from "@mantine/core";
 
 import "@mantine/core/styles.css";
@@ -22,6 +23,7 @@ declare module "@tanstack/react-router" {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <MantineProvider>
         <RouterProvider router={router} />
       </MantineProvider>
