72a0b41bc8ace0c22e878cd0eb3d92156875528f
Julio <julio.olvr@gmail.com>
2024-06-02 17:49:56 +0200

    Basic form integration

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Button, Modal, NumberInput, Stack, TextInput } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { useForm } from "@mantine/form";
 
 import { listPatterns } from "../queries/patterns";
 
@@ -15,21 +16,51 @@ function Index() {
     isNewPatternModalOpen,
     { open: openNewPatternModal, close: closeNewPatternModal },
   ] = useDisclosure();
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: { name: "", height: 30, width: 15 },
+  });
+
+  const initializeAndOpenModal = () => {
+    form.reset();
+    openNewPatternModal();
+  };
 
   return (
     <Stack>
-      <Button onClick={openNewPatternModal}>New pattern</Button>
+      <Button onClick={initializeAndOpenModal}>New pattern</Button>
       <Modal
         opened={isNewPatternModalOpen}
         onClose={closeNewPatternModal}
         title="New pattern"
       >
-        <Stack>
-          <TextInput label="Name" required />
-          <NumberInput label="Width" min={10} max={100} defaultValue={15} />
-          <NumberInput label="Height" min={10} max={100} defaultValue={30} />
-          <Button>Create</Button>
-        </Stack>
+        <form onSubmit={form.onSubmit(console.log)}>
+          <Stack>
+            <TextInput
+              label="Name"
+              required
+              key={form.key("name")}
+              {...form.getInputProps("name")}
+            />
+            <NumberInput
+              label="Width"
+              key={form.key("width")}
+              min={10}
+              max={100}
+              defaultValue={15}
+              {...form.getInputProps("width")}
+            />
+            <NumberInput
+              label="Height"
+              key={form.key("height")}
+              min={10}
+              max={100}
+              defaultValue={30}
+              {...form.getInputProps("height")}
+            />
+            <Button type="submit">Create</Button>
+          </Stack>
+        </form>
       </Modal>
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </Stack>
