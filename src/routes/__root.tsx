25444a7a0e2ebce1d84244b8726b33e0014e845c
Julio <julio.olvr@gmail.com>
2024-06-03 14:20:28 +0200

    Link to patterns list

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,16 @@
-import { AppShell } from "@mantine/core";
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { AppShell, Stack } from "@mantine/core";
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <AppShell navbar={{ width: 300, breakpoint: "sm" }} padding="md">
-        <AppShell.Navbar p="md">Test</AppShell.Navbar>
+        <AppShell.Navbar p="md">
+          <Stack>
+            <Link to="/">Patterns</Link>
+          </Stack>
+        </AppShell.Navbar>
         <AppShell.Main>
           <Outlet />
         </AppShell.Main>
