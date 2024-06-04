9fe48ca9e92c5af2d0a1a8b22cdd7e460a8473c0
Julio <julio.olvr@gmail.com>
2024-06-04 12:07:17 +0200

    Remove sidebar

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,20 +1,10 @@
-import { AppShell, Stack } from "@mantine/core";
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <AppShell navbar={{ width: 300, breakpoint: "sm" }} padding="md">
-        <AppShell.Navbar p="md">
-          <Stack>
-            <Link to="/">Patterns</Link>
-          </Stack>
-        </AppShell.Navbar>
-        <AppShell.Main>
-          <Outlet />
-        </AppShell.Main>
-      </AppShell>
+      <Outlet />
       <TanStackRouterDevtools />
     </>
   ),
