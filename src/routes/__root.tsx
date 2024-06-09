cdac5b924a5c19c8d7449443f9867e9163f9614b
Julio <julio.olvr@gmail.com>
2024-06-09 22:12:30 +0200

    Conditional router devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,16 @@
+import React from "react";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        }))
+      );
 
 export const Route = createRootRoute({
   component: () => (
