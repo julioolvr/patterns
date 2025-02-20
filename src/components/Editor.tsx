b45bf62d6c331cce3d269d8f356441f71c923d64
Julio <julio.olvr@gmail.com>
2025-02-20 16:00:51 +0100

    Custom style panel

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -2,12 +2,16 @@ import { Tldraw } from "tldraw";
 import "tldraw/tldraw.css";
 
 import { PatternShapeUtil } from "./Editor/PatternShape";
+import StylePanel from "./Editor/StylePanel";
 
 export default function App() {
   return (
     <div style={{ position: "fixed", inset: 0 }}>
       <Tldraw
         shapeUtils={[PatternShapeUtil]}
+        components={{
+          StylePanel,
+        }}
         onMount={(editor) => {
           editor.createShapes([{ type: "pattern" }]);
         }}
---
+++ src/components/Editor/StylePanel.tsx
@@ -0,0 +1,44 @@
+import { useCallback, useRef } from "react";
+import classNames from "classnames";
+import {
+  DefaultStylePanelContent,
+  TLUiStylePanelProps,
+  useEditor,
+  usePassThroughWheelEvents,
+  useRelevantStyles,
+} from "tldraw";
+
+// Copy-paste of the default style panel from tldraw, to be able to add tools
+// that don't refer to `StyleProp`s.
+export default function StylePanel({
+  isMobile,
+  children,
+}: TLUiStylePanelProps) {
+  const editor = useEditor();
+
+  const ref = useRef<HTMLDivElement>(null);
+  usePassThroughWheelEvents(ref);
+
+  const styles = useRelevantStyles();
+
+  const handlePointerOut = useCallback(() => {
+    if (!isMobile) {
+      editor.updateInstanceState({ isChangingStyle: false });
+    }
+  }, [editor, isMobile]);
+
+  const content = children ?? <DefaultStylePanelContent styles={styles} />;
+
+  return (
+    <div
+      ref={ref}
+      className={classNames("tlui-style-panel", {
+        "tlui-style-panel__wrapper": !isMobile,
+      })}
+      data-ismobile={isMobile}
+      onPointerLeave={handlePointerOut}
+    >
+      Custom style panel{content}
+    </div>
+  );
+}
