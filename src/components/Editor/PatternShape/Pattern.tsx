ebf8aaca118786e455bd4ec844ac039076ad7285
Julio <julio.olvr@gmail.com>
2025-02-20 20:56:34 +0100

    Show color counts

--- src/components/Editor/PatternShape/Pattern.tsx
+++ src/components/Editor/PatternShape/Pattern.tsx
@@ -13,34 +13,68 @@ export default function Pattern({
   // TODO: Collapse borders
   return (
     <div className="pattern">
-      {times(rows, (y) => (
-        <div
-          key={y}
-          className={classNames("pattern__row", {
-            "pattern__row--shifted": isShifted,
-          })}
-        >
-          {times(columns, (x) => (
-            <div
-              key={x}
-              className="pattern__cell"
-              style={{
-                backgroundColor: colors[y]?.[x] ?? "white",
-                transform: isShifted
-                  ? `translateX(${y % 2 === 0 ? 25 : -25}%)`
-                  : undefined,
-              }}
-              onClick={(e) => {
-                onCellClicked(x, y);
-                e.stopPropagation();
-              }}
-              onPointerDown={(e) => e.stopPropagation()}
-              onTouchStart={(e) => e.stopPropagation()}
-              onTouchEnd={(e) => e.stopPropagation()}
-            ></div>
-          ))}
-        </div>
-      ))}
+      {times(rows, (y) => {
+        let colorCount = 0;
+
+        return (
+          <div
+            key={y}
+            className={classNames("pattern__row", {
+              "pattern__row--shifted": isShifted,
+            })}
+          >
+            {times(columns, (x) => {
+              colorCount++;
+              const currentColorCount = colorCount;
+              let lastColorInARow = false;
+
+              if (x === columns - 1 || colors[y][x] !== colors[y][x + 1]) {
+                lastColorInARow = true;
+                colorCount = 0;
+              }
+
+              return (
+                <div
+                  key={x}
+                  className="pattern__cell"
+                  style={{
+                    position: "relative",
+                    backgroundColor: colors[y]?.[x] ?? "white",
+                    transform: isShifted
+                      ? `translateX(${y % 2 === 0 ? 25 : -25}%)`
+                      : undefined,
+                  }}
+                  onClick={(e) => {
+                    onCellClicked(x, y);
+                    e.stopPropagation();
+                  }}
+                  onPointerDown={(e) => e.stopPropagation()}
+                  onTouchStart={(e) => e.stopPropagation()}
+                  onTouchEnd={(e) => e.stopPropagation()}
+                >
+                  {lastColorInARow ? (
+                    <div
+                      style={{
+                        position: "absolute",
+                        top: 0,
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        fontSize: "5px",
+                        display: "inline-flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      {currentColorCount}
+                    </div>
+                  ) : null}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
