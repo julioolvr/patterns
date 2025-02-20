472ef8f904eba08c478f5144b2a0e0c1a23885f1
Julio <julio.olvr@gmail.com>
2025-02-20 16:19:03 +0100

    Show palette colors selector

--- src/components/Editor/PaletteStylePanelTool.tsx
+++ src/components/Editor/PaletteStylePanelTool.tsx
@@ -1,8 +1,14 @@
-import { useEditor } from "tldraw";
+import { TldrawUiButton, TldrawUiButtonIcon, useEditor } from "tldraw";
+import classNames from "classnames";
+
 import { PatternShape } from "./PatternShape";
+import { useState } from "react";
 
 export default function PaletteStylePanelTool() {
   const editor = useEditor();
+  // TODO: Consider that state will persist when selecting different
+  // patterns that might have different palettes
+  const [selectedColorIndex, setSelectedColorIndex] = useState(0);
   const selectedShape = editor.getOnlySelectedShape();
 
   if (
@@ -13,10 +19,25 @@ export default function PaletteStylePanelTool() {
   }
 
   return (
-    <ul>
-      {selectedShape.props.palette.map((color) => (
-        <li>{color}</li>
+    <div className={classNames("tlui-buttons__grid")}>
+      {selectedShape.props.palette.map((color, i) => (
+        <TldrawUiButton
+          type="icon"
+          key={color}
+          data-id={color}
+          aria-label={color}
+          data-state={i === selectedColorIndex ? "hinted" : undefined}
+          title={color}
+          className={classNames("tlui-button-grid__button")}
+          style={{ color }}
+          onPointerEnter={(e) => console.log({ e })}
+          onPointerDown={(e) => console.log({ e })}
+          onPointerUp={(e) => console.log({ e })}
+          onClick={() => setSelectedColorIndex(i)}
+        >
+          <TldrawUiButtonIcon icon="color" />
+        </TldrawUiButton>
       ))}
-    </ul>
+    </div>
   );
 }
