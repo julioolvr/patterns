4bf0ebbaf55771a28d3bf2d163bf7b24944b81fd
Julio <julio.olvr@gmail.com>
2025-02-21 08:18:00 -0300

    More code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Loader, MantineProvider } from "@mantine/core";
 
 import "@mantine/core/styles.css";
@@ -8,8 +6,6 @@ import "@mantine/dropzone/styles.css";
 import "@mantine/spotlight/styles.css";
 
 import { routeTree } from "./routeTree.gen";
-
-import queryClient from "./queryClient";
 
 const router = createRouter({
   routeTree,
@@ -25,12 +21,9 @@ declare module "@tanstack/react-router" {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools />
-      <MantineProvider>
-        <RouterProvider router={router} />
-      </MantineProvider>
-    </QueryClientProvider>
+    <MantineProvider>
+      <RouterProvider router={router} />
+    </MantineProvider>
   );
 }
 
